1b0ead11747ad568181203e9743bf9c55886a921
Nikolay Shabarkov <nikolayshabarkov@yandex.ru>
2020-01-18 00:47:50 +0400

    Added Replace image button for Image element

--- inc/admin/element-classes/element-scripts/image-element.js
+++ inc/admin/element-classes/element-scripts/image-element.js
@@ -6,60 +6,85 @@ a.onclick = function( e ) {
     e.preventDefault();
 };
 
-let img = element.getElementsByTagName( 'img' );
-if( img.length > 0 ) {
-    img = img[0];
-}
-
-let src;
-if( img.src ) {
-    src = img.src;
-}
-
-file_frame = wp.media.frames.file_frame = wp.media({
-    title: 'Select a image to upload',
-    button: {
-        text: 'Use this image'
-    },
-    multiple: false,
-    frame: 'post'
-});
-
-let imageSetting = function( img, attachment ) {
-    let imgSrc = attachment.url;
-    let linkArr = imgSrc.split( ':' ),
-        linkClean;
-    if ( Array.isArray( linkArr ) && linkArr.length > 0 ) {
-        linkClean = linkArr[linkArr.length - 1];
+let iconImageButton = element.getElementsByClassName( 'wptb-icon-image-button' );
+if( iconImageButton.length > 0 ) {
+    iconImageButton = iconImageButton[0];
+    
+    element.classList.add( 'wptb-element-placeholder' );
+    iconImageButton.onclick = function() {
+        addMedia( element, true );
     }
-    img.src = linkClean;
-    img.height = attachment.height;
-    img.width = attachment.width;
-    img.style.width = '100%';
-
-    let wptbTableStateSaveManager = new WPTB_TableStateSaveManager();
-    wptbTableStateSaveManager.tableStateSet();
-};
-
-file_frame.on( 'select', function() {
-    attachment = file_frame.state().props.toJSON();
-    imageSetting( img, attachment );
-});
-
-file_frame.on( 'insert', function () {
-    attachment = file_frame.state().get( 'selection' ).first().toJSON();
-    imageSetting( img, attachment );
-});
-
-if ( src == undefined ) {
-    file_frame.open();
-    file_frame.menuItemVisibility( 'gallery', 'hide' );
-    file_frame.menuItemVisibility( 'playlist', 'hide' ), 
-    file_frame.menuItemVisibility( 'video-playlist', 'hide' ), 
-    file_frame.menuItemVisibility( 'audio-playlist', 'hide' )
-} else {
-    img.src = src;
 }
+
+
+let addMedia = function( element, imageChange = false ) {
+    let img = element.querySelector( 'img' );
+
+    let src;
+    if( img && img.src ) {
+        src = img.src;
+    }
+
+    file_frame = wp.media.frames.file_frame = wp.media({
+        title: 'Select a image to upload',
+        button: {
+            text: 'Use this image'
+        },
+        multiple: false,
+        frame: 'post'
+    });
+
+    let imageSetting = function( img, attachment ) {
+        if( ! img  ) {
+            img = document.createElement( 'img' );
+            
+            let a = element.getElementsByTagName( 'a' );
+            if( a.length > 0 ) {
+                a = a[0];
+                a.innerHTML = '';
+                
+                a.appendChild( img );
+            }
+        }
+        let imgSrc = attachment.url;
+        let linkArr = imgSrc.split( ':' ),
+            linkClean;
+        if ( Array.isArray( linkArr ) && linkArr.length > 0 ) {
+            linkClean = linkArr[linkArr.length - 1];
+        }
+        img.src = linkClean;
+        img.height = attachment.height;
+        img.width = attachment.width;
+        img.style.width = '100%';
+        
+        element.classList.remove( 'wptb-element-placeholder' );
+
+        let wptbTableStateSaveManager = new WPTB_TableStateSaveManager();
+        wptbTableStateSaveManager.tableStateSet();
+    };
+
+    file_frame.on( 'select', function() {
+        attachment = file_frame.state().props.toJSON();
+        imageSetting( img, attachment );
+    });
+
+    file_frame.on( 'insert', function () {
+        attachment = file_frame.state().get( 'selection' ).first().toJSON();
+        imageSetting( img, attachment );
+    });
+
+    if ( src == undefined || imageChange == true ) {
+        file_frame.open();
+        file_frame.menuItemVisibility( 'gallery', 'hide' );
+        file_frame.menuItemVisibility( 'playlist', 'hide' ), 
+        file_frame.menuItemVisibility( 'video-playlist', 'hide' ), 
+        file_frame.menuItemVisibility( 'audio-playlist', 'hide' )
+    } else {
+        img.src = src;
+    } 
+}
+
+addMedia( element );
 
 function controlsChange( inputs, element ) {
     if( inputs && typeof inputs === 'object' ) {
@@ -70,6 +95,8 @@ function controlsChange( inputs, element ) {
                 img = img[0];
                 img.setAttribute( 'alt', imageAltValue );
             }
+        } else if( inputs.hasOwnProperty( 'imageReplaceButton' ) ) {
+            addMedia( element, true );
         }
     } 
 }
--- inc/admin/element-classes/elements/image-element.php
+++ inc/admin/element-classes/elements/image-element.php
@@ -97,6 +97,14 @@ class Image_Element extends Element_Base_Object {
 			]
 		);
         
+        $this->add_control(
+			'imageReplaceButton',
+			[
+				'label' => __( 'Replace Image', 'wp_table_builder' ),
+				'type' => Controls_Manager::BUTTON,
+			]
+		);
+        
 		$this->add_control(
 			'imageAlignmentCheckbox',
 			[
@@ -154,7 +162,26 @@ class Image_Element extends Element_Base_Object {
 	protected function _content_template() {
 		?>
         <div class="wptb-image-wrapper">
-            <a style="display: block;"><img></a>
+            <a style="display: block;">
+                <span class="wptb-icon-image-button">
+                    <svg class="wptb-draggable-prototype" xmlns="http://www.w3.org/2000/svg" 
+                         viewBox="0 0 489.4 489.4" width="40px" height="50px">
+                        <path d="M0 437.8c0 28.5 23.2 51.6 51.6 51.6h386.2c28.5 0 51.6-23.2 
+                              51.6-51.6V51.6c0-28.5-23.2-51.6-51.6-51.6H51.6C23.1 0 0 23.2 0 
+                              51.6 0 51.6 0 437.8 0 437.8zM437.8 464.9H51.6c-14.9 
+                              0-27.1-12.2-27.1-27.1v-64.5l92.8-92.8 79.3 79.3c4.8 4.8 
+                              12.5 4.8 17.3 0l143.2-143.2 107.8 107.8v113.4C464.9 452.7 452.7 
+                              464.9 437.8 464.9zM51.6 24.5h386.2c14.9 0 27.1 12.2 27.1 
+                              27.1v238.1l-99.2-99.1c-4.8-4.8-12.5-4.8-17.3 0L205.2 
+                              333.8l-79.3-79.3c-4.8-4.8-12.5-4.8-17.3 0l-84.1 84.1v-287C24.5 
+                              36.7 36.7 24.5 51.6 24.5z"/>
+                        <path d="M151.7 196.1c34.4 0 62.3-28 62.3-62.3s-28-62.3-62.3-62.3 -62.3 
+                              28-62.3 62.3S117.3 196.1 151.7 196.1zM151.7 96c20.9 0 37.8 17 37.8 
+                              37.8s-17 37.8-37.8 37.8 -37.8-17-37.8-37.8S130.8 96 151.7 96z"/>
+                    </svg>
+                    <span>Insert Image</span>
+                </span>
+            </a>
         </div>
 		<?php
 	}
