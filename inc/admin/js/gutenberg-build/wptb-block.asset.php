1e55e86fa7578d240d6d6ec27fe1dfca2ac0038b
Erdem Bircan <erdem.erdembircan@gmail.com>
2021-02-17 17:04:36 +0300

    prod build test for extra styles

--- inc/admin/js/gutenberg-build/wptb-block.asset.php
+++ inc/admin/js/gutenberg-build/wptb-block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '36a010e02879716a0685d21217e9c4fe');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '0896c04a182125c35bb804d06c6888cd');
