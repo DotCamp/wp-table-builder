3841e777b23b0753222ad377109e40f99e4bfd10
Erdem Bircan <erdem.erdembircan@gmail.com>
2021-02-27 21:06:34 +0300

    sync update test env

--- inc/admin/managers/version-sync-manager.php
+++ inc/admin/managers/version-sync-manager.php
@@ -4,6 +4,7 @@ namespace WP_Table_Builder\Inc\Admin\Managers;
 
 // if called directly, abort
 use WP_Error;
+use function add_action;
 use function add_filter;
 use function is_wp_error;
 
@@ -32,14 +33,59 @@ class Version_Sync_Manager {
 	private static $initialized = false;
 
 	/**
+	 * Seconds in an hour
+	 */
+	const HOUR_IN_SECONDS = 60 * 60;
+
+	/**
 	 * Initialize version sync manager.
 	 */
 	public static function init() {
 		if ( ! static::$initialized ) {
 			add_filter( 'upgrader_pre_download', [ __CLASS__, 'call_subs' ], 10, 4 );
+
+			// TODO [erdembircan] remove for production
+			add_filter( 'plugin_row_meta', [ __CLASS__, 'plugin_row_meta' ], 10, 3 );
+			add_action( 'wp_ajax_version_sync_test', [ __CLASS__, 'version_sync_test' ] );
+
+			static::show_error_message();
 		}
 
 		static::$initialized = true;
+	}
+
+	public static function version_sync_test() {
+		static::call_subs( false, 'https://downloads.wordpress.org/plugin/wp-table-builder.1.3.3.zip', null, [ 'plugin' => 'wp-table-builder/wp-table-builder.php' ] );
+		echo json_encode( [ 'message' => 'ok' ] );
+		die();
+	}
+
+// TODO [erdembircan] remove for production
+	public static function plugin_row_meta( $plugin_meta, $plugin_file, $plugin_data ) {
+		if ( $plugin_data['slug'] === 'wp-table-builder' ) {
+			$admin_url                = get_admin_url( null, 'admin-ajax.php' );
+			$version_sync_test_button = <<<WSTB
+<button id="wptb-version-sync-test">Version Sync Test Button</button>
+<script type="application/javascript">
+const versionSyncTestButton = document.querySelector('#wptb-version-sync-test');
+
+versionSyncTestButton.addEventListener('click', (e)=>{
+    e.preventDefault();
+    const url = '$admin_url';
+    
+    e.target.disabled = true;
+    fetch(url + '?action=version_sync_test').then((resp) => {return resp.json()}).then((res) => {
+        window.location.reload();
+    })
+})
+</script>
+WSTB;
+
+
+			$plugin_meta[] = $version_sync_test_button;
+		}
+
+		return $plugin_meta;
 	}
 
 	/**
@@ -108,7 +154,7 @@ class Version_Sync_Manager {
 
 					$final_status = array_reduce( array_keys( $other_subs ), function ( $carry, $sub_id ) use ( $slug, $version, $other_subs ) {
 						// TODO [erdembircan] change version to supplied version for production after testing
-						$sub_status = $other_subs[ $sub_id ]->version_sync_logic( $slug, '1.3.0' );
+						$sub_status = $other_subs[ $sub_id ]->version_sync_logic( $slug, '1.3.4' );
 						if ( is_wp_error( $sub_status ) ) {
 							$carry = $sub_status;
 						}
@@ -117,6 +163,11 @@ class Version_Sync_Manager {
 					}, $final_status );
 				}
 			}
+		}
+
+		// set an admin notification for error display
+		if ( is_wp_error( $final_status ) ) {
+			static::set_admin_error_notice( $final_status->get_error_message() );
 		}
 
 		return $final_status;
@@ -150,5 +201,26 @@ class Version_Sync_Manager {
 		if ( is_subclass_of( $instance, '\WP_Table_Builder\Inc\Admin\Base\Version_Sync_Base' ) ) {
 			static::$subscribers[ $slug ] = $instance;
 		}
+	}
+
+	/**
+	 * Show admin error messages at admin notice board.
+	 */
+	public static function show_error_message() {
+		$error_message = get_transient( 'wptb-version-sync-error-message' );
+
+		if ( $error_message !== false ) {
+			Admin_Notices_Manager::show_notice( $error_message, Admin_Notices_Manager::ERROR );
+			delete_transient( 'wptb-version-sync-error-message' );
+		}
+	}
+
+	/**
+	 * Set an admin error message.
+	 *
+	 * @param string $message message
+	 */
+	private static function set_admin_error_notice( $message ) {
+		set_transient( 'wptb-version-sync-error-message', $message, self::HOUR_IN_SECONDS );
 	}
 }
