43ffeea9e642c503e27f7f846051ab6563abc333
Erdem Bircan <erdem.erdembircan@gmail.com>
2021-10-15 21:52:40 +0300

    text element control updates

--- inc/admin/element-classes/elements/text-element.php
+++ inc/admin/element-classes/elements/text-element.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace WP_Table_Builder\Inc\Admin\Element_Classes\Elements;
 
 use WP_Table_Builder\Inc\Admin\Element_Classes\Base\Element_Base as Element_Base;
@@ -11,16 +12,16 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 class Text_Element extends Element_Base {
-    
-    /**
+
+	/**
 	 * Get element name.
 	 *
 	 * Retrieve text editor element name.
 	 *
+	 * @return string element name.
 	 * @since 1.1.2
 	 * @access public
 	 *
-	 * @return string element name.
 	 */
 	public function get_name() {
 		return 'text';
@@ -31,10 +32,10 @@ class Text_Element extends Element_Base {
 	 *
 	 * Retrieve text editor element title.
 	 *
+	 * @return string Element title.
 	 * @since 1.1.2
 	 * @access public
 	 *
-	 * @return string Element title.
 	 */
 	public function get_title() {
 		return esc_html_e( 'Text', 'wp-table-builder' );
@@ -45,40 +46,40 @@ class Text_Element extends Element_Base {
 	 *
 	 * Return directory text editor element icon.
 	 *
+	 * @return string directory Element icon.
 	 * @since 1.1.2
 	 * @access public
 	 *
-	 * @return string directory Element icon.
 	 */
 	public function get_directory_icon() {
-		return NS\WP_TABLE_BUILDER_DIR . 'inc/admin/views/builder/icons/text.svg'; ;
+		return NS\WP_TABLE_BUILDER_DIR . 'inc/admin/views/builder/icons/text.svg';;
 	}
-    
-    /**
+
+	/**
 	 * Get url icon.
 	 *
 	 * Return url text icon
 	 *
+	 * @return string Url Element icon.
 	 * @since 1.1.2
 	 * @access public
 	 *
-	 * @return string Url Element icon.
 	 */
 	public function get_url_icon() {
-		return wp_normalize_path ( NS\WP_TABLE_BUILDER_URL . 'inc/admin/views/builder/icons/text.svg' );
+		return wp_normalize_path( NS\WP_TABLE_BUILDER_URL . 'inc/admin/views/builder/icons/text.svg' );
 	}
-    
-    /**
+
+	/**
 	 * Include file with js script for element text
 	 *
 	 * @since 1.1.2
 	 * @access protected
 	 */
-    public function element_script() {
-        return wp_normalize_path ( NS\WP_TABLE_BUILDER_DIR . 'inc/admin/element-classes/element-scripts/text-element.js' );
-    }
-    
-    /**
+	public function element_script() {
+		return wp_normalize_path( NS\WP_TABLE_BUILDER_DIR . 'inc/admin/element-classes/element-scripts/text-element.js' );
+	}
+
+	/**
 	 * Register the element controls.
 	 *
 	 * Adds different fields to allow the user to change and customize the element settings.
@@ -91,64 +92,77 @@ class Text_Element extends Element_Base {
 		$this->add_control(
 			'section_header',
 			[
-				'label' => __( 'Text Options', 'wp_table_builder' ),
-				'type' => Controls_Manager::SECTION_HEADER,
-                'buttonBack' => true
+				'label'      => __( 'Text Options', 'wp_table_builder' ),
+				'type'       => Controls_Manager::SECTION_HEADER,
+				'buttonBack' => true
 			]
 		);
 
 		$this->add_control(
 			'color',
 			[
-				'label' => __( 'Font Color', 'wp_table_builder' ),
-				'type' => Controls_Manager::COLOR,
-                'selectors' => [
-                    '{{{data.container}}}' => 'color',
-                ]
+				'label'     => __( 'Font Color', 'wp_table_builder' ),
+				'type'      => Controls_Manager::COLOR_PALETTE,
+				'selectors' => [
+					[
+						'query' => '{{{data.container}}}',
+						'type'  => Controls_Manager::STYLE,
+						'key'   => 'color',
+					],
+				]
 			]
 		);
 
 		$this->add_control(
 			'linkColor',
 			[
-				'label' => __( 'Link Font Color', 'wp_table_builder' ),
-				'type' => Controls_Manager::COLOR,
-                'selectors' => [
-                    '{{{data.container}}} a' => 'color',
-                ]
+				'label'     => __( 'Link Font Color', 'wp_table_builder' ),
+				'type'      => Controls_Manager::COLOR_PALETTE,
+				'selectors' => [
+					[
+						'query' => '{{{data.container}}} a',
+						'type'  => Controls_Manager::STYLE,
+						'key'   => 'color',
+					]
+				]
 			]
 		);
-        
+
 		$this->add_control(
 			'size',
 			[
-				'label' => __( 'Font Size', 'wp_table_builder' ),
-				'type' => Controls_Manager::SIZE,
-                'selectors' => [
-                    '{{{data.container}}}' => 'fontSize',
-                ],
-                'min' => 10, 
-                'max' => 50,
-                'defaultValue' => 15,
-                'dimension' => 'px'
+				'label'        => __( 'Font Size', 'wp_table_builder' ),
+				'type'         => Controls_Manager::RANGE,
+				'selectors'    => [
+					[
+						'query'  => '{{{data.container}}}',
+						'type'   => Controls_Manager::STYLE,
+						'key'    => 'fontSize',
+						'format' => '{$}px'
+					]
+				],
+				'min'          => 10,
+				'max'          => 50,
+				'defaultValue' => 15,
+				'postFix'      => 'px'
 			]
 		);
 
 		$this->add_control(
 			'linkRel',
 			[
-				'label' => __( 'Link Rel', 'wp_table_builder' ),
-				'type' => Controls_Manager::MULTI_CHECKBOX,
-                'checkboxes' => [
-                        'sponsored' => 'sponsored',
-                        'nofollow' => 'nofollow',
-                        'noreferrer' => 'noreferrer',
-                ],
-				'selectors' => [
+				'label'      => __( 'Link Rel', 'wp_table_builder' ),
+				'type'       => Controls_Manager::MULTI_CHECKBOX,
+				'checkboxes' => [
+					'sponsored'  => 'sponsored',
+					'nofollow'   => 'nofollow',
+					'noreferrer' => 'noreferrer',
+				],
+				'selectors'  => [
 					[
 						'query' => '{{{data.container}}} a',
-						'type' => Controls_Manager::ATTRIBUTE,
-                        'key' => 'rel'
+						'type'  => Controls_Manager::ATTRIBUTE,
+						'key'   => 'rel'
 					]
 				],
 			]
@@ -164,7 +178,7 @@ class Text_Element extends Element_Base {
 	 * @access protected
 	 */
 	protected function _content_template() {
-	  $text_placeholder = esc_html__('Add Text', 'wp-table-builder');
+		$text_placeholder = esc_html__( 'Add Text', 'wp-table-builder' );
 		?>
         <div><p data-placeholder="<?php echo $text_placeholder; ?>"></p></div>
 		<?php
