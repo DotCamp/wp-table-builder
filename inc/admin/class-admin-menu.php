9e5ece15f463e6ee4ef37f141d61f494ceb036d8
Erdem Bircan <erdem.erdembircan@gmail.com>
2022-04-29 16:53:00 +0300

    frontend data manager

--- inc/admin/class-admin-menu.php
+++ inc/admin/class-admin-menu.php
@@ -5,6 +5,7 @@ namespace WP_Table_Builder\Inc\Admin;
 use DOMDocument;
 use WP_Table_Builder as NS;
 use WP_Table_Builder\Inc\Admin\Managers\Addon_Manager;
+use WP_Table_Builder\Inc\Admin\Managers\Frontend_Data_Manager;
 use WP_Table_Builder\Inc\Admin\Managers\Settings_Manager;
 use WP_Table_Builder\Inc\Common\Helpers;
 use WP_Table_Builder\Inc\Core\Init;
@@ -347,22 +348,22 @@ class Admin_Menu {
 				'security_code' => wp_create_nonce( 'wptb-security-nonce' ),
 				'strings'       => $strings,
 				'store'         => [
-					'tableId'      => $table_id,
-					'pro'          => Addon_Manager::check_pro_status(),
-					'translations' => [
-						'dirtyConfirmation' => esc_html__( 'You have unsaved changes, leave?', 'wp-table-builder' ),
-						'embedMessage'      => esc_html__( 'To embed this table on your site, please paste the following shortcode inside a post or page.', 'wp-table-builder' ),
-						'copyToClipboard'   => esc_html__( 'copy to clipboard', 'wp-table-builder' ),
-						'copied'            => esc_html__( 'copied', 'wp-table-builder' ),
-						'shortcode'         => esc_html__( 'shortcode', 'wp-table-builder' ),
-					]
+					'tableId' => $table_id,
+					'pro'     => Addon_Manager::check_pro_status(),
 				]
 			];
 
-			// filter for builder script data
-			$admin_object = apply_filters( 'wp-table-builder/filter/builder_script_data', $admin_object );
+			$admin_translations = [
+					'dirtyConfirmation' => esc_html__( 'You have unsaved changes, leave?', 'wp-table-builder' ),
+					'embedMessage'      => esc_html__( 'To embed this table on your site, please paste the following shortcode inside a post or page.', 'wp-table-builder' ),
+					'copyToClipboard'   => esc_html__( 'copy to clipboard', 'wp-table-builder' ),
+					'copied'            => esc_html__( 'copied', 'wp-table-builder' ),
+					'shortcode'         => esc_html__( 'shortcode', 'wp-table-builder' ),
+			];
 
-			wp_localize_script( 'wptb-controls-manager-js', 'wptb_admin_object', $admin_object );
+			Frontend_Data_Manager::add_builder_translations( $admin_translations );
+
+			Frontend_Data_Manager::localize_builder_data( 'wptb-controls-manager-js', $admin_object );
 
 			// generate controls
 			$generate_path = plugin_dir_path( __FILE__ ) . 'js/WPTB_Generate.js';
---
+++ inc/admin/managers/frontend-data-manager.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace WP_Table_Builder\Inc\Admin\Managers;
+
+use WP_Table_Builder\Inc\Common\Traits\Init_Once;
+use WP_Table_Builder\Inc\Common\Traits\Singleton_Trait;
+use function add_filter;
+use function apply_filters;
+use function wp_localize_script;
+
+/**
+ * Manager responsible for frontend data for both server and client side.
+ */
+class Frontend_Data_Manager {
+	use Singleton_Trait;
+	use Init_Once;
+
+	/**
+	 * Object name for builder data.
+	 */
+	const BUILDER_DATA_OBJECT_NAME = 'wptb_admin_object';
+
+	/**
+	 * Process given data.
+	 *
+	 * This function will be used for in situations where supplied data is a callable where returns that data.
+	 *
+	 * @param callable | array $data data
+	 *
+	 * @return array processed data
+	 */
+	private static function process_data( $data ) {
+		return is_callable( $data ) ? call_user_func( $data ) : $data;
+	}
+
+	/**
+	 * Add data to builder.
+	 *
+	 * @param array | callable $data data, or a callback function which returns data
+	 * @param String $id data id, access to data in frontend will be provided with that id
+	 *
+	 * @return void
+	 */
+	public static function add_builder_data( $data, $id = null ) {
+		add_filter( 'wp-table-builder/filter/builder_script_data', function ( $builder_data ) use ( $data, $id ) {
+			$final_data = static::process_data( $data );
+
+			if ( empty( $final_data ) ) {
+				return $builder_data;
+			}
+
+			// if no data id is supplied, merge with current builder data on top level
+			if ( is_null( $id ) ) {
+				$builder_data = array_merge( $builder_data, $final_data );
+			} else {
+				$builder_data[ $id ] = $final_data;
+			}
+
+			return $builder_data;
+		}, 10, 1 );
+	}
+
+	/**
+	 * Add translations for builder menu.
+	 *
+	 * @param array $data translation array, with keys for translation id and values as translations
+	 *
+	 * @return void
+	 */
+	public static function add_builder_translations( $data ) {
+		$static_context = __CLASS__;
+		add_filter( 'wp-table-builder/filter/builder_script_data', function ( $builder_data ) use ( $data, $static_context ) {
+			// if no translations are found, create container array
+			if ( ! isset( $builder_data['store']['translations'] ) ) {
+				$builder_data['store']['translations'] = [];
+			}
+
+			$builder_data['store']['translations'] = array_merge( $builder_data['store']['translations'], $static_context::process_data( $data ) );
+
+			return $builder_data;
+		}, 10, 1 );
+	}
+
+	/**
+	 * Localize builder data.
+	 *
+	 * @param string $script_id script id that data will be attached
+	 * @param array $data data
+	 *
+	 * @return void
+	 */
+	public static function localize_builder_data( $script_id, $data, $object_name = null ) {
+		$builder_data = apply_filters( 'wp-table-builder/filter/builder_script_data', $data );
+
+		wp_localize_script( $script_id, is_null( $object_name ) ? static::BUILDER_DATA_OBJECT_NAME : $object_name, $builder_data );
+	}
+}
