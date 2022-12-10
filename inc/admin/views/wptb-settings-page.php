2d122bc90f4e886dea5b77dfbe24c56710e41719
kevinorris <stevengerrard.mu@gmail.com>
2022-12-10 06:56:49 -0500

    remove-pagination

--- inc/admin/views/wptb-settings-page.php
+++ inc/admin/views/wptb-settings-page.php
@@ -3,13 +3,12 @@
 use WP_Table_Builder as NS;
 
 // abort at direct call
-if ( ! defined( 'WPINC' ) ) {
-	die;
+if (!defined('WPINC')) {
+    die;
 }
 
 ?>
 <div id="wptb-settings-page" class="wptb-menu-page-wrapper">
-    <settings-app :sections-data="sectionsData" :settings="settings"
-                  :plugin-info="pluginInfo">
+    <settings-app :sections-data="sectionsData" :settings="settings" :plugin-info="pluginInfo">
     </settings-app>
 </div>
