bdf6edf4fc8eb7b37a5f6b6998c38716098c70ff
Erdem Bircan <erdem.erdembircan@gmail.com>
2023-01-06 04:18:18 +0300

    1.4.6 RC

--- inc/admin/js/gutenberg-build/wptb-block.asset.php
+++ inc/admin/js/gutenberg-build/wptb-block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-polyfill'), 'version' => 'd7bbf3c83429139c90eed38db3d12115');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-polyfill'), 'version' => 'ef0885e93f9d7bf5afba019b122c0105');
