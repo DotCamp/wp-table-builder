08f3cd93058396b2516620d83ef79386352ad2e9
permafrost06 <zahin06@gmail.com>
2023-01-08 17:20:06 +0600

    added dummy upsell for progress bar element

--- inc/admin/managers/elements-manager.php
+++ inc/admin/managers/elements-manager.php
@@ -5,8 +5,8 @@ namespace WP_Table_Builder\Inc\Admin\Managers;
 use WP_Table_Builder\Inc\Core\Init;
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+if (!defined('WPINC')) {
+    die;
 }
 
 /**
@@ -18,161 +18,162 @@ if ( ! defined( 'WPINC' ) ) {
  * @since 1.1.2
  */
 class Elements_Manager extends Elements_Manager_Base {
-	/**
-	 * Elements objects.
-	 *
-	 * Holds the list of all the element objects.
-	 *
-	 * @since 1.1.2
-	 * @access protected
-	 *
-	 * @var WPTB_Element_Base_Object[]
-	 */
-	protected $_element_objects = null;
+    /**
+     * Elements objects.
+     *
+     * Holds the list of all the element objects.
+     *
+     * @since 1.1.2
+     * @access protected
+     *
+     * @var WPTB_Element_Base_Object[]
+     */
+    protected $_element_objects = null;
 
-	// element types
-	/**
-	 * elements in free version
-	 */
-	const BASIC = 'basic';
+    // element types
+    /**
+     * elements in free version
+     */
+    const BASIC = 'basic';
 
-	/**
-	 * elements in pro version
-	 */
-	const PRO = 'pro';
-
-
-	/**
-	 * Elements position on table is relative to the table data
-	 */
-	const TD_RELATIVE = 'td_relative';
-
-	/**
-	 * Element's position on table is relative to other elements
-	 */
-	const ELEMENT_RELATIVE = 'element_relative';
-
-	/**
-	 * Elements names array.
-	 *
-	 * Holds the list of all the names which have element objects.
-	 *
-	 * @since 1.1.2
-	 * @access protected
-	 *
-	 * @var array
-	 */
-	protected $_build_elements_name = [
-		'text',
-		'button',
-		'image',
-		'list',
-		'star_rating',
-		'custom_html',
-		'shortcode',
-	];
-
-	/**
-	 * Pro elements list to use in dummy display.
-	 * @var string[]
-	 */
-	protected $pro_dummy_elements_name = [
-		'circle_rating',
-		'icon',
-		'ribbon',
-		'styled_list',
-		'text_icon',
-	];
-
-	/**
-	 * Elements_Manager constructor.
-	 */
-	public function __construct() {
-		$this->_build_elements_name = apply_filters( 'wp-table-builder/filter/elements-manager-init', $this->_build_elements_name );
-	}
-
-	/**
-	 * Add new elements to already registered ones.
-	 *
-	 * @param array $new_elements array of new elements
-	 *
-	 * @return void
-	 */
-	public function add_to_elements( $new_elements ) {
-		$this->_build_elements_name = array_merge( $this->_build_elements_name, $new_elements );
-	}
+    /**
+     * elements in pro version
+     */
+    const PRO = 'pro';
 
 
-	/**
-	 * Init Elements.
-	 *
-	 * Initialize WP Table Builder Elements manager.
-	 *
-	 * @since 1.1.2
-	 * @access private
-	 */
-	public function element_elements() {
-		parent::element_elements();
+    /**
+     * Elements position on table is relative to the table data
+     */
+    const TD_RELATIVE = 'td_relative';
 
-		do_action( 'wp-table-builder/elements_registered', $this );
-	}
+    /**
+     * Element's position on table is relative to other elements
+     */
+    const ELEMENT_RELATIVE = 'element_relative';
 
-	/**
-	 * Element Object Create.
-	 *
-	 * Return Element Object. Include the necessary element files.
-	 *
-	 * @param $element_name
-	 *
-	 * @return mixed
-	 * @since 1.1.2
-	 * @access protected
-	 */
-	protected function get_element_object( $element_name ) {
-		$class_name = ucfirst( $element_name ) . '_Element';
+    /**
+     * Elements names array.
+     *
+     * Holds the list of all the names which have element objects.
+     *
+     * @since 1.1.2
+     * @access protected
+     *
+     * @var array
+     */
+    protected $_build_elements_name = [
+        'text',
+        'button',
+        'image',
+        'list',
+        'star_rating',
+        'custom_html',
+        'shortcode',
+    ];
 
-		$class_name = '\WP_Table_Builder\Inc\Admin\Element_Classes\Elements\\' . $class_name;
+    /**
+     * Pro elements list to use in dummy display.
+     * @var string[]
+     */
+    protected $pro_dummy_elements_name = [
+        'circle_rating',
+        'icon',
+        'ribbon',
+        'styled_list',
+        'text_icon',
+        'progress_bar'
+    ];
 
-		return new $class_name();
-	}
+    /**
+     * Elements_Manager constructor.
+     */
+    public function __construct() {
+        $this->_build_elements_name = apply_filters('wp-table-builder/filter/elements-manager-init', $this->_build_elements_name);
+    }
 
-	/**
-	 * Render Elements content.
-	 *
-	 * @since 1.1.2
-	 * @access public
-	 */
-	public function output_elements_templates() {
-		foreach ( $this->get_element_objects() as $element ) {
-			if ( method_exists( $element, 'output_template' ) ) {
-				$element->output_template();
-			}
-		}
-	}
+    /**
+     * Add new elements to already registered ones.
+     *
+     * @param array $new_elements array of new elements
+     *
+     * @return void
+     */
+    public function add_to_elements($new_elements) {
+        $this->_build_elements_name = array_merge($this->_build_elements_name, $new_elements);
+    }
 
-	/**
-	 * Render Elements content.
-	 *
-	 * @since 1.1.2
-	 * @access public
-	 */
 
-	public function output_directories_icons() {
-		$directories_icons = array();
-		foreach ( $this->get_element_objects() as $element ) {
-			if ( method_exists( $element, 'get_url_icon' ) ) {
-				$directories_icons[ $element->get_name() ] = $element->get_url_icon();
-			}
-		}
-		?>
+    /**
+     * Init Elements.
+     *
+     * Initialize WP Table Builder Elements manager.
+     *
+     * @since 1.1.2
+     * @access private
+     */
+    public function element_elements() {
+        parent::element_elements();
+
+        do_action('wp-table-builder/elements_registered', $this);
+    }
+
+    /**
+     * Element Object Create.
+     *
+     * Return Element Object. Include the necessary element files.
+     *
+     * @param $element_name
+     *
+     * @return mixed
+     * @since 1.1.2
+     * @access protected
+     */
+    protected function get_element_object($element_name) {
+        $class_name = ucfirst($element_name) . '_Element';
+
+        $class_name = '\WP_Table_Builder\Inc\Admin\Element_Classes\Elements\\' . $class_name;
+
+        return new $class_name();
+    }
+
+    /**
+     * Render Elements content.
+     *
+     * @since 1.1.2
+     * @access public
+     */
+    public function output_elements_templates() {
+        foreach ($this->get_element_objects() as $element) {
+            if (method_exists($element, 'output_template')) {
+                $element->output_template();
+            }
+        }
+    }
+
+    /**
+     * Render Elements content.
+     *
+     * @since 1.1.2
+     * @access public
+     */
+
+    public function output_directories_icons() {
+        $directories_icons = array();
+        foreach ($this->get_element_objects() as $element) {
+            if (method_exists($element, 'get_url_icon')) {
+                $directories_icons[$element->get_name()] = $element->get_url_icon();
+            }
+        }
+?>
         <script type="text/html" id="tmpl-wptb-element-icons-directories">
-			<?php echo json_encode( $directories_icons ); ?>
+            <?php echo json_encode($directories_icons); ?>
         </script>
 
         // code javascript for for preloading icons
         <script type="text/javascript">
-            (function () {
-                window.onload = function () {
+            (function() {
+                window.onload = function() {
                     let wptbElementIconsDirectories = 'wptb-element-icons-directories';
                     let tmplIconsDirectories = wp.template(wptbElementIconsDirectories);
                     let data = {};
@@ -187,24 +188,24 @@ class Elements_Manager extends Elements_Manager_Base {
                 };
             })();
         </script>
-		<?php
-	}
+<?php
+    }
 
-	/**
-	 * Get registered element controls.
-	 * @return array|null elements' control stack
-	 */
-	public function element_control_stack() {
-		return Init::instance()->controls_manager->get_current_control_stack();
-	}
+    /**
+     * Get registered element controls.
+     * @return array|null elements' control stack
+     */
+    public function element_control_stack() {
+        return Init::instance()->controls_manager->get_current_control_stack();
+    }
 
-	/**
-	 * Set control stack for given element type.
-	 *
-	 * @param string $element_type table element type
-	 * @param array $control_stack element control stack
-	 */
-	public function set_element_control_stack( $element_type, $control_stack ) {
-		Init::instance()->controls_manager->set_element_stack( $element_type, $control_stack );
-	}
+    /**
+     * Set control stack for given element type.
+     *
+     * @param string $element_type table element type
+     * @param array $control_stack element control stack
+     */
+    public function set_element_control_stack($element_type, $control_stack) {
+        Init::instance()->controls_manager->set_element_stack($element_type, $control_stack);
+    }
 }
--- inc/admin/managers/upsells-manager.php
+++ inc/admin/managers/upsells-manager.php
@@ -9,8 +9,8 @@ use function add_filter;
 use function esc_html__;
 use function wp_sprintf;
 
-if ( ! defined( 'WPINC' ) ) {
-	die();
+if (!defined('WPINC')) {
+    die();
 }
 
 /**
@@ -21,206 +21,206 @@ if ( ! defined( 'WPINC' ) ) {
  */
 class Upsells_Manager {
 
-	/**
-	 * Pro elements list to use in dummy display.
-	 * @var string[]
-	 */
-	protected static $pro_dummy_elements_name = [
-		'circle_rating',
-		'icon',
-		'ribbon',
-		'styled_list',
-		'text_icon',
-	];
+    /**
+     * Pro elements list to use in dummy display.
+     * @var string[]
+     */
+    protected static $pro_dummy_elements_name = [
+        'circle_rating',
+        'icon',
+        'ribbon',
+        'styled_list',
+        'text_icon',
+        'progress_bar'
+    ];
 
-	/**
-	 * Pro controls list to use in dummy display.
-	 * @var string[]
-	 */
-	protected static $pro_dummy_controls = [
-		'control_spacing_dummy'
-	];
-
-
-	/**
-	 * Upsells messages
-	 * @var array
-	 */
-	protected static $upsell_messages = [];
-
-	/**
-	 * Initialize manager.
-	 */
-	public static function init() {
-		if ( ! Addon_Manager::check_pro_status() ) {
-			static::$upsell_messages = [
-				'after_elements'  => [
-					'message' => esc_html__( 'For More Advanced Elements', 'wp-table-builder' ),
-					'url'     => 'https://wptablebuilder.com/pricing/?utm_source=dashboard&utm_medium=elements-section&utm_campaign=wptb'
-				],
-				'generate_menu'   => [
-					'message' => esc_html__( 'For Prebuilt Tables and Using Your Own Tables as Prebuilt Tables', 'wp-table-builder' ),
-					'url'     => 'https://wptablebuilder.com/pricing/?utm_source=dashboard&utm_medium=generate-menu&utm_campaign=wptb'
-				],
-				'cell_management' => [
-					'message' => esc_html__( 'For More Advanced Options in Cell Management Mode', 'wp-table-builder' ),
-					'url'     => 'https://wptablebuilder.com/pricing/?utm_source=dashboard&utm_medium=cell-management&utm_campaign=wptb'
-				],
-				'generic_end'     => wp_sprintf( '<br><div>%s %s %s</div>', esc_html_x( 'Get the', 'start of the "Get the Pro Add-On" sentence', 'wp-table-builder' ), '<span class="wptb-upsells-pro-label">PRO</span>', esc_html__( 'Add-On.', 'wp-table-builder' ) )
-			];
-
-			// @deprecated
-//			add_action( 'wp-table-builder/action/after_cell_notselected_left_panel', [
-//				__CLASS__,
-//				'cell_management_upsell'
-//			], 10, 1 );
-
-			// @deprecated
-//			add_action( 'wp-table-builder/action/cell_option', [
-//				__CLASS__,
-//				'cell_management_upsell'
-//			], 10, 1 );
-
-			add_filter( 'wp-table-builder/filter/generate_data', [ __CLASS__, 'generate_data_filter' ], 1, 1 );
-
-			Frontend_Data_Manager::add_builder_data( [ __CLASS__, 'builder_data' ], 'upsells' );;
-		}
-	}
+    /**
+     * Pro controls list to use in dummy display.
+     * @var string[]
+     */
+    protected static $pro_dummy_controls = [
+        'control_spacing_dummy'
+    ];
 
 
-	/**
-	 * Initialize upsells manager at main init thread.
-	 * @return void
-	 */
-	public static function independent_init() {
-		if ( ! Addon_Manager::check_pro_status() ) { // initialize upsell pro controls
-			Upsells_Dummy_Controls_Manager::init( [ 'dummy_controls' => static::$pro_dummy_controls ], '\WP_Table_Builder\Inc\Admin\Managers\Upsells_Dummy_Controls_Manager' );// upsell pro elements
+    /**
+     * Upsells messages
+     * @var array
+     */
+    protected static $upsell_messages = [];
 
-            if(Init::instance()->elements_manager){
-	            Init::instance()->elements_manager->add_to_elements(static::$pro_dummy_elements_name);
+    /**
+     * Initialize manager.
+     */
+    public static function init() {
+        if (!Addon_Manager::check_pro_status()) {
+            static::$upsell_messages = [
+                'after_elements'  => [
+                    'message' => esc_html__('For More Advanced Elements', 'wp-table-builder'),
+                    'url'     => 'https://wptablebuilder.com/pricing/?utm_source=dashboard&utm_medium=elements-section&utm_campaign=wptb'
+                ],
+                'generate_menu'   => [
+                    'message' => esc_html__('For Prebuilt Tables and Using Your Own Tables as Prebuilt Tables', 'wp-table-builder'),
+                    'url'     => 'https://wptablebuilder.com/pricing/?utm_source=dashboard&utm_medium=generate-menu&utm_campaign=wptb'
+                ],
+                'cell_management' => [
+                    'message' => esc_html__('For More Advanced Options in Cell Management Mode', 'wp-table-builder'),
+                    'url'     => 'https://wptablebuilder.com/pricing/?utm_source=dashboard&utm_medium=cell-management&utm_campaign=wptb'
+                ],
+                'generic_end'     => wp_sprintf('<br><div>%s %s %s</div>', esc_html_x('Get the', 'start of the "Get the Pro Add-On" sentence', 'wp-table-builder'), '<span class="wptb-upsells-pro-label">PRO</span>', esc_html__('Add-On.', 'wp-table-builder'))
+            ];
+
+            // @deprecated
+            //			add_action( 'wp-table-builder/action/after_cell_notselected_left_panel', [
+            //				__CLASS__,
+            //				'cell_management_upsell'
+            //			], 10, 1 );
+
+            // @deprecated
+            //			add_action( 'wp-table-builder/action/cell_option', [
+            //				__CLASS__,
+            //				'cell_management_upsell'
+            //			], 10, 1 );
+
+            add_filter('wp-table-builder/filter/generate_data', [__CLASS__, 'generate_data_filter'], 1, 1);
+
+            Frontend_Data_Manager::add_builder_data([__CLASS__, 'builder_data'], 'upsells');;
+        }
+    }
+
+
+    /**
+     * Initialize upsells manager at main init thread.
+     * @return void
+     */
+    public static function independent_init() {
+        if (!Addon_Manager::check_pro_status()) { // initialize upsell pro controls
+            Upsells_Dummy_Controls_Manager::init(['dummy_controls' => static::$pro_dummy_controls], '\WP_Table_Builder\Inc\Admin\Managers\Upsells_Dummy_Controls_Manager'); // upsell pro elements
+
+            if (Init::instance()->elements_manager) {
+                Init::instance()->elements_manager->add_to_elements(static::$pro_dummy_elements_name);
             }
-		}
-	}
+        }
+    }
 
-	/**
+    /**
      * @deprecated
-	 * Add pro dummy elements to element stack.
-	 *
-	 * @param array $all_elements all available table elements
-	 *
-	 * @return array table element
-	 */
-	public static function add_pro_dummy_elements( $all_elements ) {
-		if ( ! Addon_Manager::check_pro_status() ) {
-			return array_merge( $all_elements, static::$pro_dummy_elements_name );
-		}
+     * Add pro dummy elements to element stack.
+     *
+     * @param array $all_elements all available table elements
+     *
+     * @return array table element
+     */
+    public static function add_pro_dummy_elements($all_elements) {
+        if (!Addon_Manager::check_pro_status()) {
+            return array_merge($all_elements, static::$pro_dummy_elements_name);
+        }
 
-		return $all_elements;
-	}
+        return $all_elements;
+    }
 
 
-	/**
-	 * Add upsell related data to builder scripts.
-	 *
-	 * @return array filtered builder data
-	 */
-	public static function builder_data() {
-		return [
-			'pro'      => Addon_Manager::check_pro_status(),
-			'elements' => [
-				'leftPanel' => static::after_elements_upsell()
-			]
-		];
-	}
+    /**
+     * Add upsell related data to builder scripts.
+     *
+     * @return array filtered builder data
+     */
+    public static function builder_data() {
+        return [
+            'pro'      => Addon_Manager::check_pro_status(),
+            'elements' => [
+                'leftPanel' => static::after_elements_upsell()
+            ]
+        ];
+    }
 
-	/**
-	 * Get specific upsell data with the given data.
-	 *
-	 * @param string $data_key data key
-	 *
-	 * @return array upsell data
-	 */
-	private static function get_upsell_data( $data_key ) {
-		if ( key_exists( $data_key, static::$upsell_messages ) ) {
-			return static::$upsell_messages[ $data_key ];
-		}
+    /**
+     * Get specific upsell data with the given data.
+     *
+     * @param string $data_key data key
+     *
+     * @return array upsell data
+     */
+    private static function get_upsell_data($data_key) {
+        if (key_exists($data_key, static::$upsell_messages)) {
+            return static::$upsell_messages[$data_key];
+        }
 
-		return [];
-	}
+        return [];
+    }
 
-	/**
-	 * Upsell for cell management menu.
-	 */
-	public static function cell_management_upsell() {
-		extract( static::get_upsell_data( 'cell_management' ) );
-		static::prepare_upsell_element( $message, $url );
-	}
+    /**
+     * Upsell for cell management menu.
+     */
+    public static function cell_management_upsell() {
+        extract(static::get_upsell_data('cell_management'));
+        static::prepare_upsell_element($message, $url);
+    }
 
-	/**
-	 * Add upsells to generate menu.
-	 *
-	 * @param array $generate_data generate data
-	 *
-	 * @return array modified generate data
-	 */
-	public static function generate_data_filter( $generate_data ) {
-		extract( static::get_upsell_data( 'generate_menu' ) );
-		$upsell_element = static::prepare_upsell_element( $message, $url, false );
+    /**
+     * Add upsells to generate menu.
+     *
+     * @param array $generate_data generate data
+     *
+     * @return array modified generate data
+     */
+    public static function generate_data_filter($generate_data) {
+        extract(static::get_upsell_data('generate_menu'));
+        $upsell_element = static::prepare_upsell_element($message, $url, false);
 
-		$generate_data['upsell'] = $upsell_element;
+        $generate_data['upsell'] = $upsell_element;
 
-		return $generate_data;
-	}
+        return $generate_data;
+    }
 
-	/**
-	 * Prepare upsell element for display.
-	 *
-	 * @param string $message message
-	 * @param string $url url for the message
-	 * @param boolean $echo whether echo to buffer or get string representation of upsell element
-	 */
-	public static final function prepare_upsell_element( $message, $url, $echo = true ) {
-		if ( ! Addon_Manager::check_pro_status() ):
-			ob_start();
-			?>
-            <a class="wptb-upsells-anchor" href="<?php echo esc_url( $url ); ?>"
-               target="_blank">
+    /**
+     * Prepare upsell element for display.
+     *
+     * @param string $message message
+     * @param string $url url for the message
+     * @param boolean $echo whether echo to buffer or get string representation of upsell element
+     */
+    public static final function prepare_upsell_element($message, $url, $echo = true) {
+        if (!Addon_Manager::check_pro_status()) :
+            ob_start();
+?>
+            <a class="wptb-upsells-anchor" href="<?php echo esc_url($url); ?>" target="_blank">
                 <div class="wptb-upsells-wrapper">
-                    <div class="wptb-upsells-message-holder wptb-plugin-box-shadow-md"><?php echo join( ' ', [
-							"<div>$message</div>",
-							static::$upsell_messages['generic_end']
-						] ); ?></div>
+                    <div class="wptb-upsells-message-holder wptb-plugin-box-shadow-md"><?php echo join(' ', [
+                                                                                            "<div>$message</div>",
+                                                                                            static::$upsell_messages['generic_end']
+                                                                                        ]); ?></div>
                 </div>
             </a>
-			<?php
-			if ( $echo ) {
-				ob_end_flush();
-			} else {
-				return ob_get_clean();
-			}
-		endif;
+<?php
+            if ($echo) {
+                ob_end_flush();
+            } else {
+                return ob_get_clean();
+            }
+        endif;
 
-		return '';
-	}
+        return '';
+    }
 
-	/**
-	 * Check status of pro version.
-	 * This check will return true only if a valid license is activated for pro version.
-	 * @return bool pro enabled or not
-	 * @deprecated
-	 */
-	public static function check_pro_status() {
-		return class_exists( '\WP_Table_Builder_Pro\Inc\Admin\Managers\Freemius_Manager' ) && filter_var( Freemius_Manager::is_premium_activated(), FILTER_VALIDATE_BOOLEAN );
-	}
+    /**
+     * Check status of pro version.
+     * This check will return true only if a valid license is activated for pro version.
+     * @return bool pro enabled or not
+     * @deprecated
+     */
+    public static function check_pro_status() {
+        return class_exists('\WP_Table_Builder_Pro\Inc\Admin\Managers\Freemius_Manager') && filter_var(Freemius_Manager::is_premium_activated(), FILTER_VALIDATE_BOOLEAN);
+    }
 
-	/**
-	 * Upsell notification after elements at left panel.
-	 *
-	 * @return string upsell elements
-	 */
-	public static function after_elements_upsell() {
-		extract( static::get_upsell_data( 'after_elements' ) );
+    /**
+     * Upsell notification after elements at left panel.
+     *
+     * @return string upsell elements
+     */
+    public static function after_elements_upsell() {
+        extract(static::get_upsell_data('after_elements'));
 
-		return static::prepare_upsell_element( $message, $url, false );
-	}
+        return static::prepare_upsell_element($message, $url, false);
+    }
 }
