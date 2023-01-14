c4dda19e870b9d999b377325d70ee4e426476961
Erdem Bircan <erdem.erdembircan@gmail.com>
2023-01-14 18:31:07 +0300

    v1.4.7 RC

--- wp-table-builder.php
+++ wp-table-builder.php
@@ -15,7 +15,7 @@
  * Plugin Name:       WP Table Builder
  * Plugin URI:        https://wptablebuilder.com/
  * Description:       Drag and Drop Responsive Table Builder Plugin for WordPress.
- * Version:           1.4.7-dev
+ * Version:           1.4.7
  * Author:            WP Table Builder
  * Author URI:        https://wptablebuilder.com//
  * License:           GPL-3.0+
