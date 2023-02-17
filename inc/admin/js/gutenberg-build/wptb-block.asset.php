1970fa3c8784e3a08b3544ba8a099d7e5715fc7a
Imtiaz Rayhan <irayhan.asif@gmail.com>
2023-02-16 20:08:04 -0500

    Prod build

--- inc/admin/js/gutenberg-build/wptb-block.asset.php
+++ inc/admin/js/gutenberg-build/wptb-block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-polyfill'), 'version' => 'ef0885e93f9d7bf5afba019b122c0105');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-polyfill'), 'version' => '3719b523a730e3a2f2035fa87f901b40');
