9dbb1f5b7f7c6bca2969163a9f57a25bce2fdd32
Zahin Azmayeen <zahin06@gmail.com>
2023-03-22 21:40:19 +0600

    cleaned up code and added new functionality added constants for meta keys added solid backup and restore separate load_tables function added table version check and update

--- inc/admin/managers/database-updater.php
+++ inc/admin/managers/database-updater.php
@@ -2,6 +2,7 @@
 
 namespace WP_Table_Builder\Inc\Admin\Managers;
 
+use WP_Table_Builder as NS;
 use WP_Table_Builder\Inc\Common\Traits\Init_Once;
 use WP_Table_Builder\Inc\Common\Traits\Singleton_Trait;
 use WP_Query;
@@ -13,16 +14,27 @@ class Database_Updater
     use Singleton_Trait;
     use Init_Once;
 
+    const TABLE_META_KEY = '_wptb_content_';
+    const BACKUP_META_KEY = '_wptb_content_backup_';
+    const SOLID_BACKUP_META_KEY = '_wptb_content_solid_backup_';
+
     public static function init_process()
     {
         add_action("wp_ajax_update_all", array(__CLASS__, 'ajax_update_all_tables'));
         add_action("wp_ajax_get_tables_num", array(__CLASS__, 'ajax_num_tables'));
         add_action("wp_ajax_restore_tables", array(__CLASS__, 'ajax_restore_tables'));
+        add_action("wp_ajax_restore_solid_tables", array(__CLASS__, 'ajax_restore_solid_tables'));
+    }
+
+    public static function ajax_restore_solid_tables()
+    {
+        static::restore_tables_from_backup(true);
+        wp_send_json_success();
     }
 
     public static function ajax_restore_tables()
     {
-        static::restore_tables();
+        static::restore_tables_from_backup();
         wp_send_json_success();
     }
 
@@ -37,7 +49,7 @@ class Database_Updater
         wp_send_json_success();
     }
 
-    public static function update_all_tables()
+    public static function load_tables($meta_key = static::TABLE_META_KEY)
     {
         global $post;
 
@@ -48,14 +60,38 @@ class Database_Updater
 
         $query = new WP_Query($args);
 
+        $tables = [];
+
         while ($query->have_posts()) {
             $query->the_post();
-            $old_table = get_post_meta($post->ID, '_wptb_content_', true);
-            static::create_backup($post->ID, $old_table);
-            $newTable = static::update_table($old_table);
-            update_post_meta($post->ID, '_wptb_content_', $newTable);
+            $tables[$post->ID] = get_post_meta($post->ID, $meta_key, true);
         }
         wp_reset_postdata();
+
+        return $tables;
+    }
+
+    public static function update_all_tables()
+    {
+        $tables = static::load_tables();
+
+        if (get_option('wptb_solid_backups') == false) static::create_solid_table_backups();
+
+        foreach ($tables as $id => $old_table) {
+            $version = static::get_table_version($old_table);
+
+            if ($version !== NS\PLUGIN_VERSION) {
+                static::create_table_backup($id, $old_table);
+                $newTable = static::update_table($old_table);
+                update_post_meta($id, '_wptb_content_', $newTable);
+            }
+        }
+    }
+
+    public static function get_table_version($table)
+    {
+        if (preg_match('/data-wptb-version="([\d.]*)"/', $table, $matches))
+            return $matches[1];
     }
 
     public static function update_table($table)
@@ -67,40 +103,54 @@ class Database_Updater
         Table_Updates\Empty_Anchor::update($table);
         Table_Updates\Button_Element::update($table);
 
+        static::update_table_version($table);
+
         return static::get_clean_table($table);
     }
 
-    public static function create_backup($id, $old_table)
+    public static function update_table_version(&$table)
     {
-        if (empty(get_post_meta($id, '_wptb_content_backup_', true)))
-            update_post_meta($id, '_wptb_content_backup_', $old_table);
+        $table->querySelector('table')->setAttribute('wptb-table-version', NS\PLUGIN_VERSION);
+        return $table;
     }
 
-    public static function restore_tables()
+    public static function create_table_backup($id, $old_table)
     {
-        global $post;
+        if (empty(get_post_meta($id, static::BACKUP_META_KEY, true)))
+            update_post_meta($id, static::BACKUP_META_KEY, $old_table);
+    }
 
-        $args = array(
-            'post_type' => 'wptb-tables',
-            'posts_per_page' => -1
-        );
+    public static function create_solid_table_backups()
+    {
+        $tables = static::load_tables();
 
-        $query = new WP_Query($args);
+        foreach ($tables as $id => $table) {
+            update_post_meta($id, static::SOLID_BACKUP_META_KEY, $table);
+        }
 
-        while ($query->have_posts()) {
-            $query->the_post();
-            $backup_table = get_post_meta($post->ID, '_wptb_content_backup_', true);
+        add_option('wptb_solid_backups', true);
+    }
 
-            if ($backup_table !== false) {
-                update_post_meta($post->ID, '_wptb_content_', $backup_table);
-                delete_post_meta($post->ID, '_wptb_content_backup_');
-            }
+    public static function restore_tables_from_backup($solid = false)
+    {
+        $key = ($solid) ? static::SOLID_BACKUP_META_KEY : static::BACKUP_META_KEY;
+
+        $tables = static::load_tables($key);
+
+        foreach ($tables as $id => $backup_table) {
+            if ($backup_table == false || $backup_table == "") continue;
+
+            update_post_meta($id, '_wptb_content_', $backup_table);
+
+            if (!$solid)
+                delete_post_meta($id, static::BACKUP_META_KEY);
         }
         wp_reset_postdata();
     }
 
-    public static function get_clean_table($html_string)
+    public static function get_clean_table($dom)
     {
-        return explode("</body>", explode("<body>", $html_string->__toString())[1])[0];
+        return explode("</body>", explode("<body>", $dom->__toString())[1])[0];
+        // return $dom->querySelector('body')->innerHTML;
     }
 }
