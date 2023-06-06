1c250d42d5b5cdeecb150efda6f867360f30c1cc
Zahin Azmayeen <zahin06@gmail.com>
2023-06-06 23:44:32 +0600

    version bump 1.4.10 and prod build

--- inc/admin/js/gutenberg-build/wptb-block.asset.php
+++ inc/admin/js/gutenberg-build/wptb-block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-polyfill'), 'version' => '3719b523a730e3a2f2035fa87f901b40');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-polyfill'), 'version' => '5d1a708663c0e03e8594ff93172df297');
