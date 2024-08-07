cbc4ac23423b96dddd58126e647cb7312cf082b4
Zahin Azmayeen <zahin06@gmail.com>
2024-08-07 16:48:10 +0600

    add DOM-Sanitizer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dotcamp/wp-table-builder',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '09bc438d0f9a871406f9d92cc27ba21a89fee37b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dotcamp/wp-table-builder' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '09bc438d0f9a871406f9d92cc27ba21a89fee37b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'rhukster/dom-sanitizer' => array(
+            'pretty_version' => '1.0.7',
+            'version' => '1.0.7.0',
+            'reference' => 'c2a98f27ad742668b254282ccc5581871d0fb601',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../rhukster/dom-sanitizer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
