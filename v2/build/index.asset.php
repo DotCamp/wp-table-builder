87ad2580860363d8c0244e055be051babd0e0589
Saad <sakib.saad.khan@gmail.com>
2025-01-30 12:32:09 +0600

    v2-rebuild

--- v2/build/index.asset.php
+++ v2/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '234fdbb875f2aacc5716');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '193680777ed74eecca83');
