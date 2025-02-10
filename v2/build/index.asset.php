518f77431cdfd824d1d8456fd2783b836b2a5a5d
Saad <sakib.saad.khan@gmail.com>
2025-02-10 22:02:56 +0600

    v2 rebuild

--- v2/build/index.asset.php
+++ v2/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'bc1b34e0a03f8ce3184c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'f59653d8ab47cd26dbda');
