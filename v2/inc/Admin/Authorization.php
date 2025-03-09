f3b1b571c3f64aad93f2b33570ca539c4baa137d
Saad <sakib.saad.khan@gmail.com>
2025-03-09 13:00:00 +0600

    v2 change

--- v2/inc/Admin/Authorization.php
+++ v2/inc/Admin/Authorization.php
@@ -2,11 +2,16 @@
 
 namespace WPTableBuilder\Admin;
 
-class Authorization {
-    public static function can_edit() {
-        $allowed = get_option('wptb_allowed_roles', [
-            'administrator',
-        ]);
+class Authorization
+{
+    public static function can_edit()
+    {
+        $options = get_option('wp_table_builder_settings', []);
+        if (isset($options['allowed_roles']) && is_array($options['allowed_roles'])) {
+            $allowed = $options['allowed_roles'];
+        } else {
+            $allowed = ['administrator'];
+        }
         $user = wp_get_current_user();
         $common = array_intersect($allowed, $user->roles);
         return sizeof($common) > 0;
